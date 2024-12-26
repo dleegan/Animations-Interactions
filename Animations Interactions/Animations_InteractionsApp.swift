4716f5e0e7d04c713d2057c5f98bcd0f409aed00
DUPROS Leegan <leegan.dupros@epitech.eu>
2024-12-26 23:53:19 +0100

    Initial Commit

---
+++ Animations Interactions/Animations_InteractionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animations_InteractionsApp.swift
+//  Animations Interactions
+//
+//  Created by dleegan on 26/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Animations_InteractionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
